c096dfd0fcafe7c1b3184c7f2c72ee7eb1c5fb59
bdowds <bwdowds@gmail.com>
2019-03-07 18:27:05 -0600

    Added instructions to Program class

--- SolveArrays/Program.cs
+++ SolveArrays/Program.cs
@@ -6,7 +6,14 @@ namespace SolveArrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            /*
+             * 1. Create a branch in git and make your commits on that branch
+             * 2. Open the ArraySolver class and implement the four methods provided.
+             * 3. Run the Tests provided by clicking the Test tab at the top of Visual Studio and run all tests
+             * 4. If you pass all tests then you can push your branch back to the GitHub repository
+             * 
+             * Remember to make commits throughout!
+             */ 
             Console.ReadLine();
         }
     }
